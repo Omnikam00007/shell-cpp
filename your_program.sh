8accaad67a82b1446e0b9b5474c7eb41f9ccf26d
Om Nikam <omnikam00007@gmail.com>
2026-07-16 23:19:46 +0530

    codecrafters submit [skip ci]

--- your_program.sh
+++ your_program.sh
@@ -5,18 +5,35 @@
 # Note: Changing this script WILL NOT affect how CodeCrafters runs your program.
 #
 # Learn more: https://codecrafters.io/program-interface
+error_trap() {
+  echo "\n❌ [ERROR] Script failed during compilation or setup!"
+  echo "Press [ENTER] to close this window and see what broke..."
+  read dummy_variable
+}
 
-set -e # Exit early if any commands fail
+# Bind the function to the ERR signal so it catches any crash
+trap 'error_trap' EXIT
 
+
+
+set +e # Exit early if any commands fail
+# set -x
 # Copied from .codecrafters/compile.sh
 #
 # - Edit this to change how your program compiles locally
 # - Edit .codecrafters/compile.sh to change how your program compiles remotely
+
+
+
 (
   cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
   cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
   cmake --build ./build
 )
+
+trap - EXIT
+
+# set +e
 
 # Copied from .codecrafters/run.sh
 #
