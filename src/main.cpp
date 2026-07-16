055b471ddfe63ae00fc9a03e2aca0dd570bf566c
Om Nikam <omnikam00007@gmail.com>
2026-07-17 00:12:03 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,10 @@ int main() {
     std::string command;
     std::getline(std::cin,command);
     std::cout<<command<<": command not found\n";
+    if(command=="exit"){
+      std::cout<<"$ exit"<<std::endl;
+      exit(0);
+    }
   }
 
   return 0;
