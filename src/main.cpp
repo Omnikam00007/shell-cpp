ba9cb72fbe08193c96ca84f7ce56f9e53b85fc18
Om Nikam <omnikam00007@gmail.com>
2026-07-18 00:29:42 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,9 @@ int main() {
     if(command=="exit"){
       break;
     }
+    if(command.substr(0,5)=="echo"){
+      std::cout<<command.substr(0,command.size()-1)
+    }
     std::cout<<command<<": command not found\n";
   }
 
